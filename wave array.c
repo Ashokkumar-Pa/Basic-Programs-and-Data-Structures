613c33bf084d18dc19dfbed6b1f1c80dd6e5a6d4
Ashokkumar <84506412+imashokma@users.noreply.github.com>
2021-05-26 11:20:38 +0530

    Create wave array.c

---
+++ wave array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int wave[] = {10,5,3,11,2,20,22,9,1};
+	int len = sizeof wave/sizeof (wave[0]);
+	int i, j;
+    
+	for( i=0; i<len; i++ )
+	{
+		for( j=i+1 ; j<len; j++ )
+		{
+			if( i%2==0 && wave[i] > wave[j] )
+			{
+				int temp=wave[j];
+				wave[j]=wave[i];
+				wave[i]=temp;
+			}
+		}
+	}
+    
+	for( i=0; i<len; i++ )
+	{
+		printf("%d ", wave[i] );
+	}
+}
+ 
