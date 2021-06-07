060aff7027e4af28350e29006d6fbc73af49eb2a
Ashokkumar <84506412+imashokma@users.noreply.github.com>
2021-06-07 16:08:23 +0530

    Update String Palindrome.c

--- String Palindrome.c
+++ String Palindrome.c
@@ -2,22 +2,29 @@
 #include<string.h>
 int main()
 {
-	char string[] = "a man, a plan, a canal: panama";
+	char string[] = "A Man, a plan, a CanaL: Panama";
 	int len =strlen(string);
 	int start=0;
 	int end= len-1;
 	while(start<end)
 	{
-		if(string[start]==string[end])
+		if(string[start]==string[end] )
 		{
 			start++;
 			end--;
+		}
+		else if( string[start]>='A' && string[start]<='Z')	// Uppercase to Lowercase
+		{
+			string[start]  += 32;
+		}
+		else if( string[end]>='A' && string[end]<='Z')		// Uppercase to Lowercase
+		{
+			string[end] +=32;
 		}
 		else if(string[start]<'a' || string[start]==' ')
 		{
 			start++;
 		}
-		
 		else if(string[end]<'a' || string[end]==' ')
 		{
 			end--;
