3d19c63836671460d086c603c0a424267edab28a
Ashokkumar <84506412+imashokma@users.noreply.github.com>
2021-06-11 18:34:28 +0530

    Create Single Number.c

---
+++ Single Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+	int n[]={1,1,2,2,4,4,5,5,7};
+	int len =sizeof n/sizeof n[0];
+	int i, j;
+	int freq[10]={0};
+	for(i=0; i<len; i++)
+    {
+    	freq[ n[i] ]++;
+	}
+	for(j=0; j<len; j++)
+	{
+		if(freq[j]==1)
+		{
+			printf("%d ",j);
+			break;		
+		}		
+	}	
+}
