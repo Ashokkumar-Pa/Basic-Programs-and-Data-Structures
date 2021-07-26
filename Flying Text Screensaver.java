cc16bc693eb451c2475537808d6826a67cb9c6f5
Ashokkumar <84506412+imashokma@users.noreply.github.com>
2021-07-26 18:16:09 +0530

    Create Flying Text Screensaver.java

---
+++ Flying Text Screensaver.java
@@ -0,0 +1,89 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        int width = 9, height  = 3, lenght = 4;
+        boolean right = true, down = true, left = false, up = false;
+        int x = 0, y = 0;
+        int a = 0;
+        while (a != 15)
+        {
+            a++;
+            System.out.println(x + " " + y);
+            if (right && down)
+            {
+                if (height == y + 1)
+                {
+                    down = false;
+                    up = true;
+                }
+                else if (width == (x + lenght ))
+                {
+                    right = false;
+                    left = true;
+                }
+                else
+                {
+                    x++;
+                    y++;
+                }
+            }
+            else if (up && right)
+            {
+                if ( y == 0)
+                {
+                    up = false;
+                    down = true;
+                }
+                else if (width == (x + lenght ))
+                {
+                    right = false;
+                    left = true;
+                }
+                else
+                {
+                    x++;
+                    y--;
+                }
+            }
+            else if(up && left)
+            {
+                if ( y == 0)
+                {
+                    up = false;
+                    down = true;
+                }
+                else if (x == 0)
+                {
+                    left = false;
+                    right = true;
+                }
+                else
+                {
+                    x--;
+                    y--;
+                }
+            }
+            else if (down && left)
+            {
+                if (height == y + 1)
+                {
+                    down = false;
+                    up = true;
+                }
+                else if (x == 0)
+                {
+                    left = false;
+                    right = true;
+                }
+                else
+                {
+                    x--;
+                    y++;
+                }
+            }
+        }
+    }
+}
+
+
